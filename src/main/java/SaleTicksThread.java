bf528c329d9169efbd5a80d47db1d37bcdb58a27
ren_jianxiong <ren_jianxiong@163.com>
2018-04-17 00:15:25 +0800

    1、项目创建，上传到github 2、卖票业务提交

---
+++ src/main/java/SaleTicksThread.java
@@ -0,0 +1,18 @@
+/**
+ * 卖票业务，多线程练习
+ */
+public class SaleTicksThread {
+
+    public static void main(String[] args) {
+        Ticks1 t1 = new Ticks1();
+        new Thread(t1,"线程1").start();
+    }
+}
+
+
+class Ticks1 implements Runnable{
+    public void run() {
+        System.out.println("卖票了");
+    }
+}
+
